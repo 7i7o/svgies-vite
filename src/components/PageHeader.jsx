22972e1c0a2718805afcf3a127eaf1c9d4bec876
7i7o <mparij@gmail.com>
2022-08-07 14:29:54 -0300

    Update Info on page header

--- src/components/PageHeader.jsx
+++ src/components/PageHeader.jsx
@@ -23,7 +23,7 @@ const PageHeader = (props) => {
                             SVGies are a unique visual representation of your wallet address <br />
                             You can only mint ONE because they are a 1:1 representation of your wallet <br />
                             {/* They are stored on-chain in the Polygon mainnet network <br /> */}
-                            They are stored on-chain in Ethereum mainnet network <br />
+                            They are stored on-chain in the Ethereum mainnet network as a Soulbound NFT<br />
                             You can think of them as Blockies that evolved to SVGs (Scalable Vector Graphics never get pixelated) <br />
                             Drawn as generative art in a beautifully detailed and colorful SVG <br />
                             Using Quadratic and Cubic Bézier curves and leveraging our brain&apos;s ability to remember vertical simmetry <br />
